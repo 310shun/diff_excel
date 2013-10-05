1e0f978a369950151a89d016e7e6915e1974604b
asiram <msairniksua@gmail.com>
2013-10-05 22:45:43 +0900

    book_to_json_hash complete

--- lib/excel_book.rb
+++ lib/excel_book.rb
@@ -27,12 +27,32 @@ class ExcelBook < Excel
 			sheet_count = 0
 			while book.worksheet(sheet_count)
 				sheet = book.worksheet(sheet_count)
+				book_hash[sheet.name] = sheet_to_json_array(sheet)
+				sheet_count += 1
 			end
-
+debugger
 			return book_hash
 		end
 
-		def sheet_to_json_hash
+		def sheet_to_json_array(sheet)
+			first_used_row, first_unused_row, first_used_col, first_unused_col = sheet.dimensions
+			# sheet_hash = Hash.new
+			# sheet_hash[sheet.name] = Array.new
+			row_array = Array.new #sheet_hash[sheet.name]
+			header_array = sheet.row(first_used_row).to_a
 
+			header_count = 0
+			((first_used_row+1)..first_unused_row-1).each do |row_index|
+				row = sheet.row(row_index)
+				row_array << Hash.new
+				(first_used_col..first_unused_col-1).each do |col_index|
+					row_array.last[header_array[header_count]] = row[col_index]
+					header_count += 1
+				end
+
+				header_count = 0
+			end
+
+			return row_array
 		end
 end
--- spec/lib/excel_book_spec.rb
+++ spec/lib/excel_book_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
+require 'excel_book'
 
 describe 'lib/excel_book' do
-
+	context "" do
+		it "tekito" do
+			@excel_book = ExcelBook.new('public/spread_test.xls')
+			#expect("tako").to eq("tako")
+		end
+	end
 end
